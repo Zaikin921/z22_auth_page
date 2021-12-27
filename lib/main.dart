71634af203b6eee8e9dc8f56c7b2a6a74cdb1b9f
Danil Egupov <danil.egupov@gmail.com>
2021-12-27 18:34:09 +0700

    Добавлены фон и изображение на форме

--- lib/main.dart
+++ lib/main.dart
@@ -21,18 +21,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
     home: Scaffold(
         body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/img/bg.jpg'),
+              fit: BoxFit.cover,
+            )
+          ),
           width: double.infinity,
-          child: Column(
-            children: [
-              SizedBox(height: 83,),
-              SizedBox(width: 110,height: 83, child: Placeholder(),),
-              SizedBox(height: 20,),
-              Text("Введите логин в виде 10 цифр номера телефона",
-                style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6)),),
-              SizedBox(height: 20,),
-              SizedBox(
-                width: 224,
-                child: TextField(
+          height: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 70),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: 150,),
+                SizedBox(width: 110,height: 83,
+                  child: Image(image: AssetImage('assets/img/logo.png'),),),
+                SizedBox(height: 20,),
+                Text("Введите логин в виде 10 цифр номера телефона",
+                  style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6)),),
+                SizedBox(height: 20,),
+                TextField(
+                  keyboardType: TextInputType.phone,
                   decoration: InputDecoration(
                     filled: true,
                     fillColor: Color(0xFFeceff1),
@@ -41,11 +50,8 @@ class MyApp extends StatelessWidget {
                     labelText: "Телефон",
                   ),
                 ),
-              ),
-              SizedBox(height: 20,),
-              SizedBox(
-                width: 224,
-                child: TextField(
+                SizedBox(height: 20,),
+                TextField(
                   obscureText: true,
                   decoration: InputDecoration(
                     filled: true,
@@ -55,28 +61,28 @@ class MyApp extends StatelessWidget {
                     labelText: "Пароль",
                   ),
                 ),
-              ),
-              SizedBox(height: 30,),
-              Container(
-                width: 180,
-                height: 45,
-                child: ElevatedButton(
-                    onPressed: (){},
-                  child: Text("Войти"),
-                  style: ElevatedButton.styleFrom(
-                    primary: Color(0xFF0079D0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(22.0),
-                    )
-                  ),),
-              ),
-              SizedBox(height: 60,),
-              InkWell(child: const Text("Регистрация", style: linkTextStyle),
-                onTap: (){},),
-              SizedBox(height: 20,),
-              InkWell(child: const Text("Забыли пароль?", style: linkTextStyle),
-                onTap: (){},),
-            ],
+                SizedBox(height: 30,),
+                Container(
+                  width: 180,
+                  height: 45,
+                  child: ElevatedButton(
+                      onPressed: (){},
+                    child: Text("Войти"),
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF0079D0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(22.0),
+                      )
+                    ),),
+                ),
+                SizedBox(height: 60,),
+                InkWell(child: const Text("Регистрация", style: linkTextStyle),
+                  onTap: (){},),
+                SizedBox(height: 20,),
+                InkWell(child: const Text("Забыли пароль?", style: linkTextStyle),
+                  onTap: (){},),
+              ],
+            ),
           ),
         )
     ),
